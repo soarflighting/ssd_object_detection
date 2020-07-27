5755905a299945a31fb5b0e1bd0f2a4212b3a78d
soarflight <15133054746@163.com>
2020-07-28 00:23:12 +0800

    Add files via upload

---
+++ create_data_list.py
@@ -0,0 +1,7 @@
+from utils import *
+
+
+if __name__ == '__main__':
+    output_folder = "./data/"
+    voc2007_path = "d:/input_data/VOCtrainval_06-Nov-2007/VOCdevkit/VOC2007"
+    create_data_lists(output_folder,voc2007_path)
